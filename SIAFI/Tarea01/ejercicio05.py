1a3854eb01e18c501ce2cde3026e3552057e8872
Emiliano Mendoza <cemh.fi@comunidad.unam.mx>
2024-10-05 19:39:43 -0600

    Del SIAFI

--- SIAFI/Tarea01/ejercicio05.py
+++
@@ -1,9 +0,0 @@
-"""Completa el siguiente código para sólo imprimir los números pares
-
-Código a completar:
-for i in range(1,101):
-"""
-
-for i in range(1,101):
-    if i & 1 == 0:
-        print(i, end=' ')
